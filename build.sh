cc41993c283f0409f6e3e4f5293767d1fb878ee1
Ruslan Sendecky <Ruslan.Sendecky@nab.com.au>
2020-10-02 18:36:09 +1000

    Cleanup build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 podman build -t code -f blueprint --squash-all . && \
 # podman pod create --name devine --hostname devine --network host && \
-podman run -itd -u ruslan --userns=keep-id --privileged --name code --hostname code --network host -v /data/devel:/home/ruslan/devel code && \
-podman run --pod devine -d --name postgres -e POSTGRES_PASSWORD=postgres postgres:13-alpine
+podman run -itd -u ruslan --userns=keep-id --privileged --name code --hostname code --network host -v /data/devel:/home/ruslan/devel code
+#podman run --pod devine -d --name postgres -e POSTGRES_PASSWORD=postgres postgres:13-alpine
 
