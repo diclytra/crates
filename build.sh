959da618b39c32504f239bf234a2647c0e374ff4
Ruslan Sendecky <r.sendecky@gmail.com>
2020-10-07 21:10:37 +1100

    Add variables and deploy script

--- build.sh
+++ build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-podman build -t code -f blueprint --squash-all . && \
-# podman pod create --name devine --hostname devine --network host && \
-podman run -itd -u ruslan --userns=keep-id --privileged --name code --hostname code --network host -v /data/devel:/home/ruslan/devel code
-#podman run --pod devine -d --name postgres -e POSTGRES_PASSWORD=postgres postgres:13-alpine
+. ./variables
+
+podman build --rm --squash-all \
+--build-arg USERID=$USERID \
+--build-arg VERS=$VERS \
+--build-arg NAME=$NAME \
+--build-arg UNAME=$UNAME \
+-f blueprint.$BASE -t $NAME:$VERS .
+
+podman tag $NAME:$VERS $NAME:latest
+
+if [[ $? -eq 0 ]];then
+  printf "$VERS" > version
+fi
 
