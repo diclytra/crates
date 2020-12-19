3655f625a058be8132d3b9b9eae1a38e2408dc87
Ruslan Sendecky <r.sendecky@gmail.com>
2020-12-20 05:15:35 +1100

    add fish to tmux

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 . ./variables
 
-docker run -itd --hostname $NAME --name $NAME -v /home/rsendecky/code:/home/ruslan/code -p 1234:1234 $NAME
+docker run -itd --hostname $NAME --name $NAME -v /home/rsendecky/code:/home/ruslan/code -p 1234:1234 -p 7777:7777 -p 8888:8888 $NAME
 
