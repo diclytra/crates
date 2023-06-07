b9b1bf8a8c09b26af7677129b5ec6795428ceb13
Nero Dicentra <nero.dicentra@gmail.com>
2023-06-07 21:40:53 +1000

    Add ssh config

--- make.sh
+++ make.sh
@@ -28,13 +28,17 @@ function build {
 
 function deploy {
   echo "info: deploying base $TYPE with name $NAME"
-  local TGT=$HOME/$SHELF
-  mkdir -p $TGT
+  local TGT=$HOME/$MOUNT
+  local SEC=$HOME/.groot
+  mkdir -p $TGT && chmod -R 700 $TGT
+  mkdir -p $SEC/ssh && chmod -R 700 $SEC
   $EXEC run \
   -itd \
+  --userns=keep-id \
   --hostname $NAME \
   --name $NAME \
-  -v $TGT:/home/$UIDN/$SHELF \
+  -v $TGT:/home/$UIDN/$MOUNT \
+  -v $SEC:/home/$UIDN/.groot \
   -p 1234:1234 -p 7777:7777 -p 8888:8888 \
   $NAME
 }
